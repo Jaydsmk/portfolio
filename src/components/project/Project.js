6e58a9ed7fae1c4aa4b97276b5edb092c770a057
jaydsmk <jaydsmk@gmail.com>
2021-11-03 12:14:05 -0600

    modified project component

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./Project.css";
+import binoo from "../../img/binoo_website.jpg";
 
 export default function Project() {
   return (
     <div className="project" id="project">
-      Project
+      <h1>Project</h1>
+      <ul>
+        <li className="active">Portfolio</li>
+        <li>Binoo - E Commerce Website</li>
+        <li>Daily 50 Projects</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          {/* <img src={binoo} alt="binoo-ECommerce Web" /> */}
+          <h3 className="title">Binoo - E Commerce</h3>
+        </div>
+        <div className="item">
+          <img src={binoo} alt="binoo-ECommerce Web" />
+          <h3 className="title">Binoo - E Commerce</h3>
+        </div>
+        <div className="item">
+          <img src={binoo} alt="binoo-ECommerce Web" />
+          <h3 className="title">Binoo - E Commerce</h3>
+        </div>
+      </div>
     </div>
   );
 }
