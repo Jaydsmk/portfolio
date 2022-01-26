c28f1abdcb40461a6829caf38ba9621ab2e5b25c
Jayden(Seongmin) Kim <jaydsmk@gmail.com>
2022-01-26 16:02:25 -0700

    Revised project component

--- src/component/works/Works.jsx
+++ src/component/works/Works.jsx
@@ -1,13 +1,39 @@
 import './works.scss';
 import { useState } from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Works() {
 
     const [currentSlide, setCurrentSlide] = useState(0);
 
     const data = [
+
         {
             id: 1,
+            icons: [
+                {
+                    image: process.env.PUBLIC_URL + "/images/react.png"
+                },
+                {
+                    image: process.env.PUBLIC_URL + "/images/sass.png"
+                },
+            ],
+            title: "My website",
+            desc: "My portfolio website with React and Scss",
+            img: process.env.PUBLIC_URL + "/images/myWeb.jpg",
+            links: [
+                {
+                    address: "https://jaydsmk.github.io/portfolio/",
+                    linkIcon: process.env.PUBLIC_URL + "/images/link.svg"
+                },
+                {
+                    address: "https://github.com/Jaydsmk/portfolio",
+                    linkIcon: process.env.PUBLIC_URL + "/images/github.svg"
+                }
+            ]
+        },
+                {
+            id: 2,
             icons: [
                 {
                     image: process.env.PUBLIC_URL + "/images/mongoDB.png"
@@ -23,22 +49,14 @@ export default function Works() {
                 }
             ],
             title: "E-commerce",
-            desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum, aperiam.",
-            img: process.env.PUBLIC_URL + "/images/rsz_binoo.png",
-        },
-        {
-            id: 2,
-            icons: [
+            desc: "A typical shopping website with basic functions made of MERN(MongoDB,Express,React and NodeJS) stacks.",
+                    img: process.env.PUBLIC_URL + "/images/rsz_binoo.png",
+            links: [
                 {
-                    image: process.env.PUBLIC_URL + "/images/react.png"
+                    address: "http://binooandbuffalo.herokuapp.com",
+                    linkIcon: process.env.PUBLIC_URL + "/images/link.svg"
                 },
-                {
-                    image: process.env.PUBLIC_URL + "/images/sass.png"
-                },
-            ],
-            title: "My website",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, exercitationem?",
-            img: process.env.PUBLIC_URL + "/images/myWeb.jpg",
+            ]
         },
         {
             id: 3,
@@ -54,8 +72,18 @@ export default function Works() {
                 }
             ],
             title: "50 Projects",
-            desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, magni!",
+            desc: "This is 50 mini projects in HTML, CSS & JavaScript that relate to the DOM (Document Object Model).",
             img: process.env.PUBLIC_URL + "/images/50Projects.jpg",
+            links: [
+                {
+                    address: "https://jaydsmk.github.io/50-Projects/",
+                    linkIcon: process.env.PUBLIC_URL + "/images/link.svg"
+                },
+                {
+                    address: "https://github.com/Jaydsmk/50-Projects",
+                    linkIcon: process.env.PUBLIC_URL + "/images/github.svg"
+                }
+            ]
         },
     ];
 
@@ -87,7 +115,30 @@ export default function Works() {
                                     </div>
                                     <h2>{d.title}</h2>
                                     <p>{d.desc}</p>
-                                    <span>Projects</span>
+
+                                    <div className="btnContainer">
+                                        {d.links.map(link => (
+                                            <button className="linkBtn">
+                                            <a href={link.address} target="_blank" rel="noopener noreferrer">
+                                                <img src={link.linkIcon} alt="myWebsite" />
+                                            </a>
+                                        </button>
+                                        ))}       
+                                    </div>
+
+                                    {/* <div className="btnContainer">
+                                        <button className="linkBtn">
+                                            <a href="http://binooandbuffalo.herokuapp.com" target="_blank" rel="noopener noreferrer">
+                                                <img src={process.env.PUBLIC_URL + "/images/link.svg"} alt="myWebsite" />
+                                            </a>
+                                        </button>
+                                        <button className="linkBtn">
+                                            <a href="http://binooandbuffalo.herokuapp.com" target="_blank" rel="noopener noreferrer">
+                                                <img src={process.env.PUBLIC_URL + "/images/github.svg"} alt="Ecommerce" />
+                                            </a>
+                                        </button>
+                                    </div> */}
+
                                 </div>
                             </div>
                             <div className="right">
